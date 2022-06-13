fd16cffcfbdedc64e4d5af5dcdad9ca82a3e3677
rivques <38469076+rivques@users.noreply.github.com>
2022-06-13 19:12:46 -0400

    Revert "try to use static rulesets (it didn't work)"
    This reverts commit 7bd84303674cbf3db34beb200797d59deb89767b.

--- frontend/prototypes/disambig-blocker/popup.js
+++ frontend/prototypes/disambig-blocker/popup.js
@@ -3,13 +3,17 @@ let blockButton = document.getElementById("block");
 
 allowButton.addEventListener("click", async () => {
     console.log("allowing")
-    chrome.declarativeNetRequest.updateEnabledRulesets({
-        disableRulesetIds: ["block_disambig"]
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [1]
     });
 });
 blockButton.addEventListener("click", async () => {
     console.log("blocking");
-    chrome.declarativeNetRequest.updateEnabledRulesets({
-        enableRulesetIds: ["block_disambig"]
+    chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: [{
+            id: 1,
+            action: {type: "redirect", redirect: {url: "http://google.com/gen_204"}},
+            condition: {domains: ["wikipedia.org"], urlFilter: "(disambiguation)", "resourceTypes": ["main_frame"]}
+        }]
     });
 });
