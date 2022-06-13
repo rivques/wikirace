6b06f957f5a45da671e4093eb4708fe11135f2bc
rivques <38469076+rivques@users.noreply.github.com>
2022-06-13 19:25:02 -0400

    [REVERT] we're just gonna block disambigs for now
    This reverts commit 761e10becae8412991c002082f6d0fed380fbde4.

--- frontend/prototypes/disambig-blocker/popup.js
+++ frontend/prototypes/disambig-blocker/popup.js
@@ -1,47 +1,19 @@
-let dAllowButton = document.getElementById("d-allow");
-let dBlockButton = document.getElementById("d-block");
+let allowButton = document.getElementById("allow");
+let blockButton = document.getElementById("block");
 
-dAllowButton.addEventListener("click", async () => {
+allowButton.addEventListener("click", async () => {
     console.log("allowing")
     chrome.declarativeNetRequest.updateDynamicRules({
         removeRuleIds: [1]
     });
 });
-dBlockButton.addEventListener("click", async () => {
+blockButton.addEventListener("click", async () => {
     console.log("blocking");
     chrome.declarativeNetRequest.updateDynamicRules({
         addRules: [{
             id: 1,
-            priority: 4,
             action: {type: "redirect", redirect: {url: "http://google.com/gen_204"}},
             condition: {domains: ["wikipedia.org"], urlFilter: "(disambiguation)", "resourceTypes": ["main_frame"]}
         }]
-    });
-});
-
-let eAllowButton = document.getElementById("e-allow");
-let eBlockButton = document.getElementById("e-block");
-
-eAllowButton.addEventListener("click", async () => {
-    console.log("allowing")
-    chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [2, 3]
-    });
-});
-eBlockButton.addEventListener("click", async () => {
-    console.log("blocking");
-    chrome.declarativeNetRequest.updateDynamicRules({
-        addRules: [{
-            "id": 2,
-            "priority": 3,
-            "action": {"type": "allow"},
-            "condition": {"urlFilter": "*wikipedia.org*", "resourceTypes": ["main_frame"]}
-        },
-        {
-            "id": 3,
-            "priority": 2,
-            "action": {"type": "redirect", "redirect": {"url": "http://google.com/gen_204"}},
-            "condition": {"initiatorDomains": ["wikipedia.org"], "resourceTypes": ["main_frame"]}
-        }] 
     });
 });
