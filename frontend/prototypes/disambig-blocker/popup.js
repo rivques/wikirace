2f06ff0b3f5cb4551fb000bbed66d5356c6f4b60
rivques <38469076+rivques@users.noreply.github.com>
2022-06-13 19:05:40 -0400

    more changes ig

--- frontend/prototypes/disambig-blocker/popup.js
+++ frontend/prototypes/disambig-blocker/popup.js
@@ -3,17 +3,13 @@ let blockButton = document.getElementById("block");
 
 allowButton.addEventListener("click", async () => {
     console.log("allowing")
-    chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [1]
+    chrome.declarativeNetRequest.updateEnabledRulesets({
+        disableRulesetIds: ["block_disambig"]
     });
 });
 blockButton.addEventListener("click", async () => {
     console.log("blocking");
-    chrome.declarativeNetRequest.updateDynamicRules({
-        addRules: [{
-            id: 1,
-            action: {type: "redirect", redirect: {url: "http://google.com/gen_204"}},
-            condition: {domains: ["wikipedia.org"], urlFilter: "(disambiguation)", "resourceTypes": ["main_frame"]}
-        }]
+    chrome.declarativeNetRequest.updateEnabledRulesets({
+        enableRulesetIds: ["block_disambig"]
     });
 });
