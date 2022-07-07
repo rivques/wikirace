abcae34e927ea73422e4e06ad20ff67300f9da5a
rivques <38469076+rivques@users.noreply.github.com>
2022-07-06 21:55:28 -0400

    half-working, having issues with tabs.getcurrent

--- frontend/wikirace/background.js
+++ frontend/wikirace/background.js
@@ -1,6 +1,7 @@
-chrome.action.onClicked.addListener(function(tab) {
+browser.browserAction.onClicked.addListener(function(tab) {
     // open a new popup window with popup.html
     chrome.tabs.create({
         url: "popup.html"
     });
 });
+console.log("background.js loaded");
